02238a6b19c56bc7a7b9251e3355931002912733
ILAN <ilan@Ilan.localdomain>
2025-07-20 21:33:49 +0200

    pwd

---
+++ exeeeeec/built_in/pwd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsadi <ilsadi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 21:24:15 by ilsadi            #+#    #+#             */
+/*   Updated: 2025/07/20 21:24:15 by ilsadi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+int	pwd(void)
+{
+	char	*cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (cwd)
+	{
+		ft_putstr_fd("pwd: error retrieving current directory: No such file or directory")
+		return (1);
+	}
+	ft_printf("%s\n", cwd)
+	free(cwd)
+	return (0);
+}
