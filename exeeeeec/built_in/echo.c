0f0584da595759c8b52a9d48eb4d665bc689b7ad
Ilan <ilansadi@icloud.com>
2025-07-20 15:21:17 +0200

    oeoeoe

---
+++ exeeeeec/built_in/echo.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilsadi <ilsadi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 15:08:33 by ilsadi            #+#    #+#             */
+/*   Updated: 2025/07/20 15:08:33 by ilsadi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+int	ft_echo(char **args)
+{
+	int	i;
+	int	j;
+	int	n_flag;
+
+	i = 1;
+	n_flag = 0;
+	while (args[i] && args[i][0] == '-' && args[i][1] == 'n')
+	{
+		j = 2;
+		while (args[i][j] == 'n')
+			j++;
+		if (args[i][j] != '\0')
+			break ;
+		n_flag = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		if (args[i + 1])
+			ft_putstr_fd(" ", 1);
+		i++;
+	}
+	if (!n_flag)
+		ft_putstr_fd("\n", 1);
+	return (0);
+}
+
