d1d33f3fd51a077d1193c33586ad87c53a5d65c2
ILAN <ilan@Ilan.localdomain>
2025-08-09 13:14:05 +0200

    oeoeoe

--- exeeeeec/input_trunc.c
+++ exeeeeec/input_trunc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   input_trunc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbrice <cbrice@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ilsadi <ilsadi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 17:12:14 by ilsadi            #+#    #+#             */
-/*   Updated: 2025/08/08 20:23:48 by cbrice           ###   ########.fr       */
+/*   Updated: 2025/08/09 13:06:53 by ilsadi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,9 @@ int	handle_redirections(t_token *tokens)
 {
 	t_token	*current;
 	int		fd;
+	int		pipefd[2];
+	char	*line;
+	int		last_heredoc_pipe = -1;
 
 	current = tokens;
 	while (current)
@@ -77,7 +80,39 @@ int	handle_redirections(t_token *tokens)
 			dup2(fd, STDOUT_FILENO);
 			close(fd);
 		}
+		else if (current->type == APPEND && current->next)
+		{
+			fd = open(current->next->str, O_CREAT | O_WRONLY | O_APPEND, 0644);
+			if (fd < 0)
+				return (perror(current->next->str), -1);
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+		}
+		else if (current->type == HEREDOC && current->next)
+		{
+			if (pipe(pipefd) == -1)
+				return (perror("pipe"), -1);
+			while (1)
+			{
+				line = readline("> ");
+				if (!line || ft_strcmp(line, current->next->str) == 0)
+					break ;
+				write(pipefd[1], line, ft_strlen(line));
+				write(pipefd[1], "\n", 1);
+				free(line);
+			}
+			free(line);
+			close(pipefd[1]);
+			if (last_heredoc_pipe != -1)
+				close(last_heredoc_pipe);
+			last_heredoc_pipe = pipefd[0];
+		}
 		current = current->next;
+	}
+	if (last_heredoc_pipe != -1)
+	{
+		dup2(last_heredoc_pipe, STDIN_FILENO);
+		close (last_heredoc_pipe);
 	}
 	return (0);
 }
